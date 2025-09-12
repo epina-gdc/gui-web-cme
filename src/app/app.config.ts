df79c1c977f3abbaa771992e56327fc08779fc0e
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-09-12 13:10:51 -0600

    configuraciones de primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import Aura from '@primeng/themes/aura';
+import {es} from "primelocale/es.json"
 import { routes } from './app.routes';
+import {providePrimeNG} from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    providePrimeNG(
+      {
+        theme: {
+          preset: Aura, options: {
+            darkModeSelector: 'none',
+            cssLayer: {
+              name: 'primeng-base',
+              order: 'primeng-base, app-components, app-overrides'
+            }
+          }
+        }, translation: es,
+      }
+    ),
+  ]
 };
