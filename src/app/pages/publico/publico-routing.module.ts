98783f5e122928cd01f0394bc1d5d7c13ce1b85b
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-09-12 13:46:28 -0600

    estilos base html, body

--- src/app/pages/publico/publico-routing.module.ts
+++ src/app/pages/publico/publico-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {InicioSesionComponent} from '@publico/inicio-sesion/inicio-sesion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'inicio-sesion',
+    pathMatch: 'full',
+  },
+  {
+    path: 'inicio-sesion',
+    component: InicioSesionComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
