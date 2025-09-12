571dbb8139fd6f45f75d489b52e227ff2ac420c7
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-09-12 13:16:55 -0600

    configuraciones de routing básico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,17 @@ export const routes: Routes = [
     redirectTo: 'publico',
     pathMatch: 'full',
   },
+  {
+    path: 'publico',
+    loadChildren: () => import('./pages/publico/publico.module').then(m => m.PublicoModule),
+  },
+  {
+    path: 'privado',
+    loadChildren: () => import('./pages/privado/privado.module').then(m => m.PrivadoModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'publico',
+    pathMatch: 'full'
+  }
 ];
