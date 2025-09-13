8927101e4aae770921ba47c09f0879dd49c5749e
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-09-12 19:07:49 -0600

    ejemplos layers

--- src/app/pages/publico/pages/inicio-sesion/inicio-sesion.component.ts
+++ src/app/pages/publico/pages/inicio-sesion/inicio-sesion.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {Card} from 'primeng/card';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'app-inicio-sesion',
   imports: [
-    Card
+    Card,
+    Button
   ],
   templateUrl: './inicio-sesion.component.html',
   styleUrl: './inicio-sesion.component.scss'
