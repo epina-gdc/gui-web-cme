a1fccbafa713323be6ed64035ed8145dd021e450
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-09-15 11:13:44 -0600

    btn regresar

--- src/app/pages/privado/pages/inicio/inicio.component.ts
+++ src/app/pages/privado/pages/inicio/inicio.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {Card} from 'primeng/card';
 import {IconCardComponent} from '../../../../components/icon-card/icon-card.component';
+import {BtnRegresarComponent} from '../../../../components/btn-regresar/btn-regresar.component';
 
 @Component({
   selector: 'app-inicio',
   imports: [
     Card,
-    IconCardComponent
+    IconCardComponent,
+    BtnRegresarComponent
   ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
