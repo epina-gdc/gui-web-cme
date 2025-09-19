eb93ee20288e3a53e61496d2a84edf8b9f215df3
Ameyalli Victoria <avictorias@live.com.mx>
2025-09-19 14:11:33 -0600

    mensajes

--- src/app/core/utils/mensajes.ts
+++ src/app/core/utils/mensajes.ts
@@ -6,5 +6,13 @@ public MSJ_SUBTITULO_CONVOCATORIA:string = "Reclutamiento IMSS 2025";
 public MSG011: string = "Ya existe una cuenta registrada con este correo electrónico...";
 public MSG012: string="Tu registro fue finalizado con éxito, se te enviará un correo con la información de tu registro.";
 public MSG013:string = "Por favor completa todos los campos obligatorios.";
+
 public MSG007:string =  "Las contraseñas no coinciden, por favor verifica tu información"
+public MSJ_PASS_MIN_CARACTER:string = "La contraseña debe tener al menos 8 caracteres"
+public MSJ_PASS_MAX_CARACTER:string = "La contraseña debe tener máximo 12 caracteres"
+public MSJ_PASS_MAYUSCULA:string = "Debe contener al menos una letra mayúscula"
+public MSJ_PASS_MINUSCULA:string = "Debe contener al menos una letra minúscula"
+public MSJ_PASS_NUMERO:string = "Debe contener al menos un número"
+public MSJ_PASS_CARACTER_ESPECIAL:string = "Debe contener al menos un caracter especial"
+
 }
