80e9216d13400dabb1fba5a1c42610b0e8da2c86
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2025-09-19 12:07:32 -0600

    Creación de archivo para guardar patrones regex

---
+++ src/app/core/utils/regex.ts
@@ -0,0 +1,4 @@
+export const PATRON_CARACTER_ESPECIAL = /[!@#$%^&*(),.?":{}|<>]/;
+export const PATRON_LETRA_MAYUSCULA = /[A-Z]/;
+export const PATRON_LETRA_MINUSCULA = /[a-z]/;
+export const PATRON_NUMERO = /\d/;
