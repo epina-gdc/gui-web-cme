f803d19e5fffd6245e7ff99e13ffd511780342ea
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2025-09-22 18:33:17 -0600

    Se agrega patrón para validar email

--- src/app/core/utils/regex.ts
+++ src/app/core/utils/regex.ts
@@ -2,4 +2,6 @@ export const PATRON_CARACTER_ESPECIAL = /[!@#$%^&*(),.?":{}|<>]/;
 export const PATRON_LETRA_MAYUSCULA = /[A-Z]/;
 export const PATRON_LETRA_MINUSCULA = /[a-z]/;
 export const PATRON_NUMERO = /\d/;
- export const  PATRON_CARACTERES_ASCII = /^[\x21-\x7E]$/;// Patrón que permite solo caracteres ASCII imprimibles sin espacios (0x21 a 0x7E)
+export const  PATRON_CARACTERES_ASCII = /^[\x21-\x7E]$/;// Patrón que permite solo caracteres ASCII imprimibles sin espacios (0x21 a 0x7E)
+export const PATRON_EMAIL = /^[a-z0-9._+%=*'-]+@[a-z0-9]+\.[a-z]{2,}$/;
+
