d537eb8e09d1184d4d4815c094b35d38318bd407
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2025-09-29 19:38:18 -0600

    Estructura integración login service

--- src/app/pages/publico/pages/inicio-sesion/inicio-sesion.component.ts
+++ src/app/pages/publico/pages/inicio-sesion/inicio-sesion.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit, signal} from '@angular/core';
+import {Component, inject, OnInit, signal, DestroyRef} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {Card} from 'primeng/card';
 import {Button} from 'primeng/button';
@@ -9,7 +9,11 @@ import {GeneralComponent} from '../../../../components/general.component';
 import {passwordValidator} from '@validators/password-validator';
 import {BloquearCaracterPasswordDirective} from '@directives/bloquear-caracter-password.directive';
 import {PATRON_EMAIL} from '@utils/regex';
+import { AuthService } from '@services/auth.service';
 
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { HttpRespuesta } from '@models/http-respuesta.interface';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -24,11 +28,17 @@ import {PATRON_EMAIL} from '@utils/regex';
   ],
   templateUrl: './inicio-sesion.component.html',
   styleUrl: './inicio-sesion.component.scss',
-  standalone: true
+  standalone: true,
+  providers: [AuthService]
 })
 export class InicioSesionComponent extends GeneralComponent implements OnInit{
 
+  loginService = inject(AuthService);
   fb = inject(FormBuilder)
+  destroyRef = inject(DestroyRef);
+
+  activatedRoute = inject(ActivatedRoute);
+
   formLogin!: FormGroup;
   vista = signal('login');
   ingresoPass: boolean = false;
@@ -43,13 +53,29 @@ export class InicioSesionComponent extends GeneralComponent implements OnInit{
 
   inicializarFormLogin() : FormGroup{
     return this.fb.group({
-      correoElectronico: ['', [Validators.required]],
+      username: ['', [Validators.required]],
       password: ['', [Validators.required, passwordValidator()]],
     });
   }
 
   iniciarSesion(){
-    console.log("");
+    if(this.formLogin.valid){
+      this.loginService.login(this.formLogin.value)
+      .subscribe({
+        next:(respuesta: HttpRespuesta<any>) => {
+          if(!respuesta.exito){
+            this._alertServices.alerta(respuesta.mensaje);  
+            return;
+          }
+          this._router.navigate(['/privado'], {
+            relativeTo: this.activatedRoute,
+          });
+        },
+        error:(error) => {
+          this._alertServices.alerta(error.error.mensaje);
+        }
+      })
+    }
   }
 
   validarCaracterCorreo(event: KeyboardEvent){
