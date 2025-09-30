3701fe48c3809dd67244c27326ae9e13ea8affe3
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-09-30 10:07:07 -0600

    ajustes configuración environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+import {ConfigEnvironment} from '@models/config-environment.interface';
+
+const base: string = 'http://10.166.120:1052/';
+
+export const environment: ConfigEnvironment = {
+  production: true,
+  api: {
+    login: base + 'mscme-autenticacion/api/'
+  }
+}
