ecc1007c5a19656a67e029a3243f52a203b01c41
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2025-10-01 12:14:19 -0600

    Validación para recuperar el payload

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,6 +2,7 @@ import {inject} from '@angular/core';
 import {CanActivateChildFn, RouterStateSnapshot} from '@angular/router';
 import {AuthService} from '@services/auth.service';
 import {Observable, of} from 'rxjs';
+import { CME_TOKEN } from '../../utils/constantes';
 
 export const authGuard: CanActivateChildFn = (childRoute, state) => {
   return checkAuth(state);
@@ -10,9 +11,9 @@ export const authGuard: CanActivateChildFn = (childRoute, state) => {
 
 const checkAuth = (state: RouterStateSnapshot): Observable<boolean> => {
   const auth = inject(AuthService);
-  if (localStorage.getItem('access_token')){
+  if (localStorage.getItem(CME_TOKEN)){
     return of(true);
-    auth.settearSession(localStorage.getItem('access_token') as string);
+    auth.settearSession(localStorage.getItem(CME_TOKEN) as string);
   }
   auth.cerrarSesion();
   return of(false);
