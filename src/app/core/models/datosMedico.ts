adb8216a150243559fe230fa1807e23509121a1f
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-10-02 17:36:45 -0600

    errores encontrados durante QA

--- src/app/core/models/datosMedico.ts
+++ src/app/core/models/datosMedico.ts
@@ -1,83 +1,69 @@
 import {CatDocumentoVerificacion, CatPerfil} from "./catalogoGeneral";
 
 export class Medico {
-    nomNombre!: string;
-    nomApellidoPaterno!: string;
-    nomApellidoMaterno!: string;
-    refCurp!: string;
-    refRfc!: string;
-    refEmail!: string;
-    refContrasenaHash!: string;
-    modalidad!: number;
-    perfil1!: number
-    nomPerfil!: string;
-perfil!: CatPerfil;
-blnInterno!: boolean;
-desDocumentoVerificacion!:string;
-documentoVerif!: CatDocumentoVerificacion;
-
+  nomNombre!: string;
+  nomApellidoPaterno!: string;
+  nomApellidoMaterno!: string;
+  refCurp!: string;
+  refRfc!: string;
+  refEmail!: string;
+  refContrasenaHash!: string;
+  modalidad!: number;
+  perfil1!: number
+  nomPerfil!: string;
+  perfil!: CatPerfil;
+  blnInterno!: boolean;
+  desDocumentoVerificacion!: string;
+  documentoVerif!: CatDocumentoVerificacion;
 }
-
-
-
 
 
 export class RegistroMedico extends Medico {
-    correo2!: string;
-    password2!: string;
-    blnPasaporte!: boolean;
-    pais!:number;
-    pasaporte!: string;
-    cveMatricula!: string;
+  correo2!: string;
+  password2!: string;
+  blnPasaporte!: boolean;
+  pais!: number;
+  pasaporte!: string;
+  cveMatricula!: string;
 }
-
 
 
 export class RegistroInternoRequest {
-   refEmail!:string;
-   refContrasenaHash!:string;
-   idPerfil!: number;
-   cveMatricula!:string;
-   nomNombre!:string;
-   nomApellidoPaterno!:string;
-   nomApellidoMaterno!:string;
-   refCurp!:string;
-   refRfc!:string;
-
-
+  refEmail!: string;
+  refContrasenaHash!: string;
+  idPerfil!: number;
+  cveMatricula!: string;
+  nomNombre!: string;
+  nomApellidoPaterno!: string;
+  nomApellidoMaterno!: string;
+  refCurp!: string;
+  refRfc!: string;
 }
 
 export class RegistroCurpRequest {
-    refEmail!: string;
-    refContrasenaHash!: string;
-    idPerfil!: number;
-    idSubperfil!: number;
-    idDocumentoVerificacion!: number;
-    nomNombre!: string;
-    nomApellidoPaterno!: string;
-    nomApellidoMaterno!: string;
-    refCurp!: string;
-    refRfc!: string;
+  refEmail!: string;
+  refContrasenaHash!: string;
+  idPerfil!: number;
+  idSubperfil!: number;
+  idDocumentoVerificacion!: number;
+  nomNombre!: string;
+  nomApellidoPaterno!: string;
+  nomApellidoMaterno!: string;
+  refCurp!: string;
+  refRfc!: string;
+}
 
-
- }
-
- export class RegistroPasaporteRequest {
-    refEmail!: string;
-    refContrasenaHash!: string;
-    idPerfil!: number;
-    idSubperfil!: number;
-    idDocumentoVerificacion!: number;
-    pasaporte!:string;
-    idPaisEmision!: number;
-    nomNombre!: string;
-    nomApellidoPaterno!: string;
-    nomApellidoMaterno!: string;
-    refCurp!: string;
-    refRfc!: string;
-
-
-
-
-
- }
+export class RegistroPasaporteRequest {
+  refEmail!: string;
+  refContrasenaHash!: string;
+  idPerfil!: number;
+  idSubperfil!: number;
+  idDocumentoVerificacion!: number;
+  pasaporte!: string;
+  idPaisEmision!: number;
+  nomNombre!: string;
+  nomApellidoPaterno!: string;
+  nomApellidoMaterno!: string;
+  refCurp!: string;
+  refRfc!: string;
+}
