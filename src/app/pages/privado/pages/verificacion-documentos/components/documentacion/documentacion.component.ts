1222c68b65053e73b211aa543ff55481fbe0f8ad
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2025-10-10 10:22:26 -0600

    componentes documentación

---
+++ src/app/pages/privado/pages/verificacion-documentos/components/documentacion/documentacion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documentacion',
+  imports: [],
+  templateUrl: './documentacion.component.html',
+  styleUrl: './documentacion.component.scss'
+})
+export class DocumentacionComponent {
+
+}
---
+++ src/app/pages/privado/pages/verificacion-documentos/verificacion-documentos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verificacion-documentos',
+  imports: [],
+  templateUrl: './verificacion-documentos.component.html',
+  styleUrl: './verificacion-documentos.component.scss'
+})
+export class VerificacionDocumentosComponent {
+
+}
