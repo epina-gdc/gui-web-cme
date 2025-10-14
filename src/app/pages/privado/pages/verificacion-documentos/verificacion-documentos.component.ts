5b0fb156ad722ead1d920d5d8e29471a5536e138
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2025-10-13 19:45:28 -0600

    Módulo verificación de documentos

--- src/app/pages/privado/pages/verificacion-documentos/verificacion-documentos.component.ts
+++ src/app/pages/privado/pages/verificacion-documentos/verificacion-documentos.component.ts
@@ -1,38 +1,54 @@
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Component, WritableSignal, inject, signal } from '@angular/core';
+import { Component, OnInit, ViewChild, WritableSignal, inject, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Card } from 'primeng/card';
 import { SelectModule } from 'primeng/select';
 import { InputText } from 'primeng/inputtext';
+import { PaginatorModule } from 'primeng/paginator';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { TableModule } from 'primeng/table';
+import { Popover, PopoverModule } from 'primeng/popover';
 import { GeneralComponent } from '../../../../components/general.component';
 import { BtnRegresarComponent } from '../../../../components/btn-regresar/btn-regresar.component';
 import { TipoDropdown } from '@models/tipo-dropdown.interface';
 import { ButtonModule } from 'primeng/button';
 import { DUMMIE_TABLA_VERIFICACION_DOCUMENTOS } from '@utils/dummies';
-import { TableModule } from 'primeng/table';
+import { EstatusDocumentacion } from '@models/verificacion-documentos.interface';
 
 @Component({
   selector: 'app-verificacion-documentos',
   imports: [
+    CommonModule,
+    ReactiveFormsModule,
     Card,
     SelectModule,
     InputText,
     TableModule,
     ButtonModule,
+    ConfirmPopupModule,
     BtnRegresarComponent,
-    ReactiveFormsModule
+    PaginatorModule,
+    PopoverModule
   ],
   templateUrl: './verificacion-documentos.component.html',
-  styleUrl: './verificacion-documentos.component.scss'
+  styleUrl: './verificacion-documentos.component.scss',
 })
-export class VerificacionDocumentosComponent extends GeneralComponent{
+export class VerificacionDocumentosComponent extends GeneralComponent implements OnInit{
+  @ViewChild('op') op!: Popover;
 
   dummies = [{ label: 'Dummie 1', value: 'Dummie 1' }, { label: 'Dummie 2', value: 'Dummie 2' }];
   dummiesTabla = DUMMIE_TABLA_VERIFICACION_DOCUMENTOS;
 
+
   filtroForm!: FormGroup;
 
   usuarioDocumentos: WritableSignal<any[]> = signal([]);
+  documentoSeleccionado: any;
+  paginaActual: number = 0;
+  first: number = 0;
+  totalElementos: number = 50;
+  rows: number = 10;
 
   especialidad: TipoDropdown[] = [];
   estatus: TipoDropdown[] = [];
@@ -40,13 +56,16 @@ export class VerificacionDocumentosComponent extends GeneralComponent{
 
   constructor(
     private readonly activatedRoute: ActivatedRoute,
-    private readonly fb: FormBuilder
-    )
+    private readonly fb: FormBuilder)
     {
       super();
       this.filtroForm = this.inicializarForm();
       this.obtenerCatalogos();
-      this.paginar();
+  }
+
+  ngOnInit(): void {
+    this.paginar();
+
   }
 
   obtenerCatalogos(){
@@ -64,8 +83,35 @@ export class VerificacionDocumentosComponent extends GeneralComponent{
     });
   }
 
+  textoEstatus(estatus: number): string {
+    return EstatusDocumentacion[estatus];
+  }
+
+  settearClase(estatus: number): string {
+    const clase = ["noCumple", "cumple", "revision", "pendiente"];
+    return clase[estatus];
+  }
+
+  onPageChange(event: any): void {
+    this.first = event.first;
+    this.rows = event.rows;
+    this.paginaActual = event.page;
+    this.paginar();
+  }
+
   paginar(){
     this.usuarioDocumentos.set(this.dummiesTabla);
+    //this.totalElementos = this.usuarioDocumentos().length;
   }
+
+
+  consultaDocumento(event: any, documento: any){
+    this.op.show(event);
+  }
+
+  hidePopover() {
+    this.op.hide();
+}
+
 
 }
