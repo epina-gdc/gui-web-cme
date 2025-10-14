16d9f9e06077ecfe04347f5df473a1b12007bf2b
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-10-14 10:59:25 -0600

    acordeon primera aproximación

--- src/app/pages/privado/pages/detalle-oferta-laboral/detalle-oferta-laboral.component.ts
+++ src/app/pages/privado/pages/detalle-oferta-laboral/detalle-oferta-laboral.component.ts
@@ -6,6 +6,7 @@ import {Button} from 'primeng/button';
 import {Tab, TabList, TabPanel, TabPanels, Tabs} from 'primeng/tabs';
 import {SplitByWidthDirective} from '@directives/split-by-width.directive';
 import {Image} from 'primeng/image';
+import {Carousel} from 'primeng/carousel';
 
 @Component({
   selector: 'app-detalle-oferta-laboral',
@@ -20,12 +21,49 @@ import {Image} from 'primeng/image';
     Tab,
     TabPanel,
     SplitByWidthDirective,
-    Image
+    Image,
+    Carousel
   ],
   templateUrl: './detalle-oferta-laboral.component.html',
   styleUrl: './detalle-oferta-laboral.component.scss'
 })
 export class DetalleOfertaLaboralComponent {
   value: any;
+  products: any[] = [{
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+  },
+    {
+      id: '1001',
+      code: 'nvklal433',
+      name: 'Black Watch',
+      description: 'Product Description',
+      image: 'black-watch.jpg',
+      price: 72,
+      category: 'Accessories',
+      quantity: 61,
+      inventoryStatus: 'OUTOFSTOCK',
+      rating: 4
+    },
+    {
+      id: '1002',
+      code: 'zz21cz3c1',
+      name: 'Blue Band',
+      description: 'Product Description',
+      image: 'blue-band.jpg',
+      price: 79,
+      category: 'Fitness',
+      quantity: 2,
+      inventoryStatus: 'LOWSTOCK',
+      rating: 3
+    }];
 
 }
