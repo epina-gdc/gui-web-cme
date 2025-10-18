ccbabcb1d941ffa28ab23cf1f306cba32a42626c
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2025-10-17 18:56:55 -0600

    Add sección doc especialidad

--- src/app/pages/privado/pages/verificacion-documentos/components/documentacion/documentacion.component.ts
+++ src/app/pages/privado/pages/verificacion-documentos/components/documentacion/documentacion.component.ts
@@ -5,10 +5,11 @@ import { TabsModule } from 'primeng/tabs';
 import { PillComponent } from '@components/pill/pill.component';
 import { BtnRegresarComponent } from '@components/btn-regresar/btn-regresar.component';
 import { DocsObligatoriosComponent } from '../docs-obligatorios/docs-obligatorios.component';
+import { DocsEspecialidadComponent } from '../docs-especialidad/docs-especialidad.component';
 
 @Component({
   selector: 'app-documentacion',
-  imports: [BtnRegresarComponent,CardModule,PillComponent,TabsModule,DocsObligatoriosComponent],
+  imports: [BtnRegresarComponent,CardModule,PillComponent,TabsModule,DocsObligatoriosComponent, DocsEspecialidadComponent],
   templateUrl: './documentacion.component.html',
   styleUrl: './documentacion.component.scss'
 })
