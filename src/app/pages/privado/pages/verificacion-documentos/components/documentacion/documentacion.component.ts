d6f58c53a53ad7e6bd02eac555763cac8070db42
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2025-10-17 19:03:34 -0600

    feat change

--- src/app/pages/privado/pages/verificacion-documentos/components/documentacion/documentacion.component.ts
+++ src/app/pages/privado/pages/verificacion-documentos/components/documentacion/documentacion.component.ts
@@ -5,8 +5,6 @@ import {TabsModule} from 'primeng/tabs';
 import {PillComponent} from '@components/pill/pill.component';
 import {BtnRegresarComponent} from '@components/btn-regresar/btn-regresar.component';
 
-import { PillComponent } from '@components/pill/pill.component';
-import { BtnRegresarComponent } from '@components/btn-regresar/btn-regresar.component';
 import { DocsObligatoriosComponent } from '../docs-obligatorios/docs-obligatorios.component';
 import { DocsEspecialidadComponent } from '../docs-especialidad/docs-especialidad.component';
 
