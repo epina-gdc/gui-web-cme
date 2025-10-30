0395ca26da6673b7f1b91653d4ee1ea44a1011d7
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2025-10-30 14:05:09 -0600

    add: estilos docs especialidad

--- src/app/pages/privado/pages/verificacion-documentos/components/docs-especialidad/docs-especialidad.component.ts
+++ src/app/pages/privado/pages/verificacion-documentos/components/docs-especialidad/docs-especialidad.component.ts
@@ -1,15 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, WritableSignal, signal } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DUMMIE_DOCS_ESPECIALIDAD } from '@utils/dummies';
 
 import { AccordionModule } from 'primeng/accordion';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { TabsModule } from 'primeng/tabs';
+import { TextareaModule } from 'primeng/textarea';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-docs-especialidad',
-  imports: [AccordionModule, RadioButtonModule, TabsModule,CommonModule,FormsModule,ReactiveFormsModule],
+  imports: [AccordionModule,
+     RadioButtonModule,
+      TabsModule,
+      CommonModule,
+      FormsModule,
+      ReactiveFormsModule,
+      TextareaModule,
+      CardModule,
+      ButtonModule
+    ],
   templateUrl: './docs-especialidad.component.html',
   styleUrl: './docs-especialidad.component.scss'
 })
@@ -19,6 +31,7 @@ export class DocsEspecialidadComponent {
   selectedTitle: string = "";
   selectedCredential: string = "";
   selectedCategory: any = null;
+  observaciones: string = "";
 
   titleOptions = [
     { label: 'Cubre', value: 'cubre' },
@@ -29,5 +42,39 @@ export class DocsEspecialidadComponent {
     { label: 'Cubre', value: 'cubre' },
     { label: 'No cubre', value: 'noCubre' }
   ];
+
+  estatusDocumentos = [
+    {label: 'Cumple con requisitos', value: 'cumple'},
+    {label: 'No cumple con requisitos', value: 'noCumple'},
+    {label: 'Revisión documental', value: 'reivison'},
+  ];
+
+  tabActive: WritableSignal<number> = signal(0);
+
+  formObservaciones!: FormGroup;
+
+
+  constructor(
+    private fb: FormBuilder,
+
+  ){
+    this.formObservaciones = this.inicializarForm()
+  }
+
+  docSeleccionado(id:number) {
+    this.tabActive.set(id);
+  }
+
+
+  inicializarForm(): FormGroup{
+
+    return this.fb.group({
+      observaciones: [{value: '', disabled: false}, [Validators.required]],
+    })
+  }
+
+  get f(){
+    return this.formObservaciones.controls;
+  }
 
 }
