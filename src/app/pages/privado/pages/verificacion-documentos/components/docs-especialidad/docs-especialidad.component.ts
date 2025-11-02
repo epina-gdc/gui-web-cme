6326513744f3b5ffcfdfc34dbc86af0147871614
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-11-02 14:55:30 -0600

    previsualización de documento

--- src/app/pages/privado/pages/verificacion-documentos/components/docs-especialidad/docs-especialidad.component.ts
+++ src/app/pages/privado/pages/verificacion-documentos/components/docs-especialidad/docs-especialidad.component.ts
@@ -1,5 +1,5 @@
 import {CommonModule} from '@angular/common';
-import {Component, Input, signal, WritableSignal} from '@angular/core';
+import {Component, inject, Input, signal, WritableSignal} from '@angular/core';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {DUMMIE_DOCS_ESPECIALIDAD} from '@utils/dummies';
 
@@ -10,6 +10,8 @@ import {TextareaModule} from 'primeng/textarea';
 import {CardModule} from 'primeng/card';
 import {ButtonModule} from 'primeng/button';
 import {DetalleDocumentacionEspecialidadDocumento} from '@models/detalleDocumentacionAspirante.interface';
+import {DocumentoService} from '@services/documentos.service';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-docs-especialidad',
@@ -30,11 +32,15 @@ export class DocsEspecialidadComponent {
 
   @Input() docsEspecialidad: DetalleDocumentacionEspecialidadDocumento[] = [];
 
+  pdfUrl: SafeResourceUrl | undefined;
+
   selectedTitle: string = "";
   selectedCredential: string = "";
   selectedCategory: any = null;
   observaciones: string = "";
   opciones: any;
+
+  documentoService = inject(DocumentoService)
 
   titleOptions = [
     {label: 'Cubre', value: 'cubre'},
@@ -59,20 +65,33 @@ export class DocsEspecialidadComponent {
 
   constructor(
     private fb: FormBuilder,
+    private sanitizer: DomSanitizer
   ) {
     this.formObservaciones = this.inicializarForm()
   }
 
-  docSeleccionado(id: number) {
+  docSeleccionado(id: number, guid: string) {
     this.tabActive.set(id);
+    this.obtenerPrevisualizacionDocumento(guid);
   }
 
-
   inicializarForm(): FormGroup {
-
     return this.fb.group({
       observaciones: [{value: '', disabled: false}, [Validators.required]],
     })
+  }
+
+  obtenerPrevisualizacionDocumento(guid: string) {
+    this.documentoService.obtenerDocumento(guid).subscribe({
+      next:(response) => {
+        const blob = new Blob([response], { type: 'application/pdf' });
+        const url = URL.createObjectURL(blob);
+        this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+      },
+      error: (err) => {
+        console.error('Error al obtener el documento', err);
+      }
+    });
   }
 
   get f() {
