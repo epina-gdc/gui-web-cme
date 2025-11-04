861ef12600047250053633a07c4c5fe96d2df59d
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2025-11-03 18:20:32 -0600

    fix: crear cuenta bug

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -55,7 +55,7 @@ export class AuthService {
     if (token) {
       this.settearSession(token);
     } else {
-      this.cerrarSesion();
+      if(this.router.url != "/publico/crear-cuenta")this.cerrarSesion();
     }
   }
 
@@ -95,6 +95,8 @@ export class AuthService {
   }
 
   cerrarSesion() {
+    debugger
+    console.trace();
     localStorage.clear();
     this.usuarioService.clearUser()
     this.usuarioSesionSubject.next(null)
