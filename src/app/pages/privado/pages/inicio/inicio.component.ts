e72b5469ca9508408904087846e17471aeb8fdad
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-11-10 09:44:08 -0600

    correcciones cierre de sesion

--- src/app/pages/privado/pages/inicio/inicio.component.ts
+++ src/app/pages/privado/pages/inicio/inicio.component.ts
@@ -312,10 +312,10 @@ export class InicioComponent extends GeneralComponent {
     this.formRegistro.get('correo')?.setValue(refEmail + '');
     this.userService.userData$.subscribe({
       next: (element) => {
-        if (!element.perfil) return;
+        if (!element) return;
         this.tipoMedico.set(element.perfil)
       }
-    })
+    });
 
   }
 
