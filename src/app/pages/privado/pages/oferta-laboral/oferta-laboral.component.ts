4832e1106b180fa1e03c34b341b5caa2a8564fc8
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2025-11-12 12:52:52 -0600

    Se quita validación visualizar archivos

--- src/app/pages/privado/pages/oferta-laboral/oferta-laboral.component.ts
+++ src/app/pages/privado/pages/oferta-laboral/oferta-laboral.component.ts
@@ -160,13 +160,12 @@ export class OfertaLaboralComponent extends GeneralComponent implements OnInit, 
     this._CatalogoGenService.getDocumentos(oportunidad.cveOoad!)
     .pipe(
       switchMap(referencias =>  {
-        if (!referencias.respuesta.sedesPdf || !referencias.respuesta.docPdf) {
-          // Si la primera llamada falla o no devuelve GUID, lanzamos un error para detener el flujo.
-          this._alertServices.alerta("No existen referencias para la OOAD seleccionada.");
-          return throwError(() => new Error(referencias?.mensaje || 'Error al obtener GUID'));
-        }
-        const pdfSede = this.documentoService.obtenerDocsPorOoad(referencias.respuesta.sedesPdf.refGuid);
-        const pdfUbicacion = this.documentoService.obtenerDocsPorOoad(referencias.respuesta.docPdf.refGuid);
+        let pdfSede;
+        let pdfUbicacion;
+        referencias.respuesta.sedesPdf ? pdfSede = this.documentoService.obtenerDocsPorOoad(referencias.respuesta.sedesPdf.refGuid) : pdfSede = of(null);
+        referencias.respuesta.docPdf ? pdfUbicacion = this.documentoService.obtenerDocsPorOoad(referencias.respuesta.docPdf.refGuid) : pdfUbicacion = of(null);
+        //pdfSede = this.documentoService.obtenerDocsPorOoad(referencias.respuesta.sedesPdf.refGuid);
+        //pdfUbicacion = this.documentoService.obtenerDocsPorOoad(referencias.respuesta.docPdf.refGuid);
         return forkJoin([of(referencias),pdfSede,pdfUbicacion])
         }
       ),
