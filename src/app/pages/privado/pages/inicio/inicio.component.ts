077b56736a5a42694acc1d60686c9bb2cc0c66b5
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-11-18 18:28:37 -0600

    ajustes medicos externos perfil 3 y 6

--- src/app/pages/privado/pages/inicio/inicio.component.ts
+++ src/app/pages/privado/pages/inicio/inicio.component.ts
@@ -232,7 +232,7 @@ export class InicioComponent extends GeneralComponent {
 
   asignarFormularioRegistro(): FormGroup {
     return this.fb.group({
-      rfc: [{value: ''} , [Validators.required, Validators.minLength(13), Validators.maxLength(13), Validators.pattern(PATRON_RFC)]],
+      rfc: [{value: ''}, [Validators.required, Validators.minLength(13), Validators.maxLength(13), Validators.pattern(PATRON_RFC)]],
       nss: [{value: '', disabled: false}, [Validators.required, Validators.minLength(11), Validators.maxLength(11)]],
       fechaNacimiento: [null, [Validators.required]],
       sexo: [{value: ''}, [Validators.required]],
@@ -342,18 +342,18 @@ export class InicioComponent extends GeneralComponent {
 
   asignarFormularioDatosEmpleo(): FormGroup {
     return this.fb.group({
-      otroEmpleo: [{value: '0', disabled: false}],
-      sustituto: [{value: '0', disabled: false}],
-      tipoInstitucion: [null],
-      nombreInstitucion: [{value: null, disabled: true}, [Validators.maxLength(200)]],
-      horarioInicio: [{value: null, disabled: true}],
-      horarioFin: [{value: null, disabled: true}],
-      diaInicio: [{value: null, disabled: true}],
-      diaFin: [{value: null, disabled: true}],
-      ooad: [{value: null, disabled: true}],
-    }
-  //  , {validators: [jornadaLaboralValidator, horarioLaboralValidator]}
-  )
+        otroEmpleo: [{value: '0', disabled: false}],
+        sustituto: [{value: '0', disabled: false}],
+        tipoInstitucion: [null],
+        nombreInstitucion: [{value: null, disabled: true}, [Validators.maxLength(200)]],
+        horarioInicio: [{value: null, disabled: true}],
+        horarioFin: [{value: null, disabled: true}],
+        diaInicio: [{value: null, disabled: true}],
+        diaFin: [{value: null, disabled: true}],
+        ooad: [{value: null, disabled: true}],
+      }
+      //  , {validators: [jornadaLaboralValidator, horarioLaboralValidator]}
+    )
   }
 
   obtenerFechaNacimientoDeCURP(curp: string): Date {
@@ -483,7 +483,7 @@ export class InicioComponent extends GeneralComponent {
       next: (valor) => {
         this.zonas = mapearArregloTipoDropdown(valor.respuesta, 'desZona', 'cveZona');
       },
-      error: ()=> {
+      error: () => {
         this.zonas = [];
         console.log('Ocurrio un error con la búsqueda de zonas');
       }
@@ -808,25 +808,25 @@ export class InicioComponent extends GeneralComponent {
       }
 
       /* CUANDO NO EXISTE CURP DEJA DE FUNCIONAR LA OBTENCION POR CURP */
-      if(perfil.idPerfil == 3){
-        if(sexo){
-        this.formRegistro.get('sexo')?.patchValue(
-          {
-            label: sexo.desSexo,
-            value: sexo.idSexo
-          }
-        )
-      } else {
-        this.formRegistro.get('sexo')?.enable();
-      }
+      if (perfil.idPerfil == 3) {
+        if (sexo) {
+          this.formRegistro.get('sexo')?.patchValue(
+            {
+              label: sexo.desSexo,
+              value: sexo.idSexo
+            }
+          )
+        } else {
+          this.formRegistro.get('sexo')?.enable();
+        }
 
-      if (fecNacimiento) {
-        var fecha = this.obtenerFechaDesdeCadena(fecNacimiento);
-        this.formRegistro.get('fechaNacimiento')?.setValue(fecha || null);
-      } else {
-        this.formRegistro.get('fechaNacimiento')?.enable();
+        if (fecNacimiento) {
+          var fecha = this.obtenerFechaDesdeCadena(fecNacimiento);
+          this.formRegistro.get('fechaNacimiento')?.setValue(fecha || null);
+        } else {
+          this.formRegistro.get('fechaNacimiento')?.enable();
+        }
       }
-    }
     }
 
     if (datosResidenciaActual) {
@@ -972,9 +972,8 @@ export class InicioComponent extends GeneralComponent {
           setTimeout(() => {
             this.indice.update((value: number) => value + 1);
           }, 500);
-        } else
-        {
-            return this._alertServices.error(data.mensaje)
+        } else {
+          return this._alertServices.error(data.mensaje)
         }
 
       },
@@ -1302,7 +1301,7 @@ export class InicioComponent extends GeneralComponent {
     const refObligatorio2 = this.documentosLocalStorageService.obtenerRefGuid(2);
     const refObligatorio3 = this.documentosLocalStorageService.obtenerRefGuid(3);
     const especialidades = this.documentosLocalStorageService.obtenerRefGuidEspecialidad();
-    const externo = this.userData?.idPerfil === 3;
+    const externo: boolean = [3, 6].includes(this.userData?.idPerfil as number);
 
     if (this.estatusPendienteDocumentacion) {
       this.indice.update((value: number) => value + 1);
@@ -1391,7 +1390,7 @@ export class InicioComponent extends GeneralComponent {
   }
 
   generarSolicitudGuardarDocumentacion(): SolicitudGuardarDocumentacion {
-    const externo = this.userData?.idPerfil === 3;
+    const externo: boolean = [3, 6].includes(this.userData?.idPerfil as number);
     const constancias = this.documentosLocalStorageService.obtenerRefConstanciaCompleta();
 
     if (!externo) {
@@ -1594,16 +1593,14 @@ export class InicioComponent extends GeneralComponent {
       return true;
     }
 
-/*     const hasTipoInstitucion = this.formDatosEmpleo.get('tipoInstitucion')?.value;
-    if(!hasTipoInstitucion){
+    /*     const hasTipoInstitucion = this.formDatosEmpleo.get('tipoInstitucion')?.value;
+        if(!hasTipoInstitucion){
 
-      return true;
-    } */
+          return true;
+        } */
     if (this.formDatosEmpleo.invalid && externo) {
       return true;
     }
-
-
 
 
     return especialidades.length === 0;
@@ -1848,8 +1845,8 @@ export class InicioComponent extends GeneralComponent {
       nombreInstitucion: datos.nomEspecificacionInstitucion || null,
 
       // Horario/Jornada
-      horarioInicio: datos.refJornadaInicio ? dayjs(datos.refJornadaInicio, 'HH:mm:ss').toDate(): null,
-      horarioFin: datos.refJornadaInicio ? dayjs(datos.refJornadaFin, 'HH:mm:ss').toDate(): null,
+      horarioInicio: datos.refJornadaInicio ? dayjs(datos.refJornadaInicio, 'HH:mm:ss').toDate() : null,
+      horarioFin: datos.refJornadaInicio ? dayjs(datos.refJornadaFin, 'HH:mm:ss').toDate() : null,
 
       // Días
       diaInicio: datos.diaSemanaInicio?.idDiaSemana ?? null,
