1a1cbc4efb3e32789d807ad957c5ac5a4a6c5ed3
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-11-19 13:32:30 -0600

    carga de primera sección -> modal -> fotografía

--- src/app/pages/privado/shared/modal-validacion-medico/modal-validacion-medico.component.ts
+++ src/app/pages/privado/shared/modal-validacion-medico/modal-validacion-medico.component.ts
@@ -1,8 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {DynamicDialogConfig} from 'primeng/dynamicdialog';
 import {DatosGeneralesResponse} from '@models/datosGenerales';
 import {TitleCasePipe} from '@angular/common';
 import {TableModule} from 'primeng/table';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import {ConvocatoriaService} from '@services/convocatoria.service';
+import {DocumentoService} from '@services/documentos.service';
 
 @Component({
   selector: 'app-modal-validacion-medico',
@@ -17,11 +20,51 @@ export class ModalValidacionMedicoComponent {
 
   datosGenerales!: DatosGeneralesResponse;
 
-  constructor(private readonly config: DynamicDialogConfig) {
+  nombreFoto!: string;
+  datosFoto!: any;
+  defaultFile!: SafeResourceUrl | undefined;
+
+  convocatoriaService: ConvocatoriaService = inject(ConvocatoriaService);
+  documentoService: DocumentoService = inject(DocumentoService);
+
+  constructor(private readonly config: DynamicDialogConfig,
+              private readonly sanitizer: DomSanitizer) {
     if (this.config.data) {
       this.datosGenerales = this.config.data.datosGenerales;
       console.log(this.config.data.datosGenerales);
     }
+  }
+
+  ngOnInit() {
+    this.obtenerDatosFoto(this.datosGenerales.datosPersonales.idUsuario);
+  }
+
+  obtenerDatosFoto(idusuario: number | undefined): void {
+    if (!idusuario) return;
+    this.convocatoriaService.getDatosFotografia(idusuario).subscribe({
+      next: (response: any) => {
+        if (!response.exito) return;
+        this.datosFoto = response.respuesta.fotografia;
+        this.obtenerFotografia()
+      }
+    });
+  }
+
+  obtenerFotografia(): void {
+    if (!this.datosFoto) return;
+    this.documentoService.getFotografia(this.datosFoto.documento.refGuid).pipe(
+    ).subscribe({
+      next: (response: any) => {
+        let extension = ['png', 'jpeg', 'jpg'];
+        if (extension.includes(this.datosFoto.documento.refExtension.toLowerCase())) {
+          this.nombreFoto = this.datosFoto.documento.refNombre;
+          const blob = new Blob([response], {type: 'blob'});
+          this.defaultFile = this.sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(blob));
+        } else {
+          this.nombreFoto = 'Sin foto'
+        }
+      }
+    });
   }
 
 
