6011ebf4a74e40fa3439ae4e7ec6ae512e6283b9
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-11-21 17:11:29 -0600

    ajustes de documentos

--- src/app/pages/privado/pages/inicio/inicio.component.ts
+++ src/app/pages/privado/pages/inicio/inicio.component.ts
@@ -1439,11 +1439,12 @@ export class InicioComponent extends GeneralComponent {
       especialidadesDocumentos
     };
     this.dialogValidacionRef = this.dialogService.open(ModalValidacionMedicoComponent, {
-      data, focusOnShow: false, resizable: true,
+      data, focusOnShow: false,
       breakpoints: {
         '960px': '75vw',
         '640px': '90vw'
       },
+      width: '80%'
     });
 
     this.dialogValidacionRef.onClose.subscribe((validacion) => {
