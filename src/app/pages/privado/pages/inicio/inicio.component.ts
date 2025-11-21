eb64db1023354cac7d246838d86374b570ea97db
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2025-11-21 16:45:03 -0600

    add: header footer

--- src/app/pages/privado/pages/inicio/inicio.component.ts
+++ src/app/pages/privado/pages/inicio/inicio.component.ts
@@ -71,6 +71,8 @@ import {DialogService, DynamicDialogRef} from 'primeng/dynamicdialog';
 import {
   ModalValidacionMedicoComponent
 } from '@pages/privado/shared/modal-validacion-medico/modal-validacion-medico.component';
+import { HeaderGenericoComponent } from '@pages/privado/shared/header-generico/header-generico.component';
+import { FooterMedicoComponent } from '@pages/privado/shared/footer-medico/footer-medico.component';
 
 interface DocumentoFuente {
   refGuid: string;
@@ -1444,6 +1446,10 @@ export class InicioComponent extends GeneralComponent {
         '960px': '75vw',
         '640px': '90vw'
       },
+      templates: {
+        header: HeaderGenericoComponent,
+        footer: FooterMedicoComponent
+      }
     });
 
     this.dialogValidacionRef.onClose.subscribe((validacion) => {
