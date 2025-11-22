245dd2defdd8ccab03ef00c1a2e1b2ca9aef77dc
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-11-21 18:22:51 -0600

    correccion de imagen

--- src/app/pages/privado/shared/footer-medico/footer-medico.component.ts
+++ src/app/pages/privado/shared/footer-medico/footer-medico.component.ts
@@ -1,5 +1,9 @@
-import {Component, inject} from '@angular/core';
-import {AuthService} from '@services/auth.service';
+import {Component, inject, OnInit} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import {ConvocatoriaService} from '@services/convocatoria.service';
+import {DocumentoService} from '@services/documentos.service';
+import {UserService} from '@services/user.service';
+import {SesionUser} from '@models/sesion-user.interface';
 
 @Component({
   selector: 'app-footer-medico',
@@ -7,9 +11,53 @@ import {AuthService} from '@services/auth.service';
   templateUrl: './footer-medico.component.html',
   styleUrl: './footer-medico.component.scss'
 })
-export class FooterMedicoComponent {
-    authService: AuthService = inject(AuthService);
+export class FooterMedicoComponent implements OnInit {
 
-    sesion = this.authService.usuarioSesion;
+  nombreFoto!: string;
+  datosFoto!: any;
+  defaultFile!: SafeResourceUrl | undefined;
 
+  convocatoriaService: ConvocatoriaService = inject(ConvocatoriaService);
+  documentoService: DocumentoService = inject(DocumentoService);
+  userService: UserService = inject(UserService);
+
+  sesion: SesionUser | null = null;
+
+  constructor(private readonly sanitizer: DomSanitizer) {
+    this.userService.userData$.subscribe(user => this.sesion = user);
+  }
+
+  ngOnInit() {
+    if (this.sesion?.idUsuario) {
+      this.obtenerDatosFoto(this.sesion.idUsuario);
+    }
+  }
+
+  obtenerDatosFoto(idusuario: number | undefined): void {
+    if (!idusuario) return;
+    this.convocatoriaService.getDatosFotografia(idusuario).subscribe({
+      next: (response: any) => {
+        if (!response.exito) return;
+        this.datosFoto = response.respuesta.fotografia;
+        this.obtenerFotografia()
+      }
+    });
+  }
+
+  obtenerFotografia(): void {
+    if (!this.datosFoto) return;
+    this.documentoService.getFotografia(this.datosFoto.documento.refGuid).pipe(
+    ).subscribe({
+      next: (response: any) => {
+        let extension = ['png', 'jpeg', 'jpg'];
+        if (extension.includes(this.datosFoto.documento.refExtension.toLowerCase())) {
+          this.nombreFoto = this.datosFoto.documento.refNombre;
+          const blob = new Blob([response], {type: 'blob'});
+          this.defaultFile = this.sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(blob));
+        } else {
+          this.nombreFoto = 'Sin foto'
+        }
+      }
+    });
+  }
 }
--- src/app/pages/privado/shared/modal-validacion-medico/modal-validacion-medico.component.ts
+++ src/app/pages/privado/shared/modal-validacion-medico/modal-validacion-medico.component.ts
@@ -1,10 +1,8 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {DynamicDialogConfig, DynamicDialogRef} from 'primeng/dynamicdialog';
 import {DatosGeneralesResponse} from '@models/datosGenerales';
 import {TitleCasePipe} from '@angular/common';
 import {TableModule} from 'primeng/table';
-import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
-import {ConvocatoriaService} from '@services/convocatoria.service';
 import {DocumentoService} from '@services/documentos.service';
 import {DatosEmpleo, DocumentoConstancia} from '@models/solicitud-guardar-documentacion.interface';
 import {DiaSemanaPipe} from '@pipes/dia-semana.pipe';
@@ -36,18 +34,12 @@ interface Especialidad {
   templateUrl: './modal-validacion-medico.component.html',
   styleUrl: './modal-validacion-medico.component.scss'
 })
-export class ModalValidacionMedicoComponent implements OnInit {
+export class ModalValidacionMedicoComponent {
 
   datosGenerales!: DatosGeneralesResponse;
   datosEmpleo!: DatosEmpleo;
   documentosConstancias: DocumentoConstancia[] = [];
   especialidades: Especialidad[] = [];
-
-  nombreFoto!: string;
-  datosFoto!: any;
-  defaultFile!: SafeResourceUrl | undefined;
-
-  convocatoriaService: ConvocatoriaService = inject(ConvocatoriaService);
   documentoService: DocumentoService = inject(DocumentoService);
   userService: UserService = inject(UserService);
 
@@ -58,7 +50,6 @@ export class ModalValidacionMedicoComponent implements OnInit {
   userData: SesionUser | null = null;
 
   constructor(private readonly config: DynamicDialogConfig,
-              private readonly sanitizer: DomSanitizer,
               public ref: DynamicDialogRef,) {
     this.userService.userData$.subscribe(user => this.userData = user);
     if (this.config.data) {
@@ -83,37 +74,6 @@ export class ModalValidacionMedicoComponent implements OnInit {
     }
   }
 
-  ngOnInit() {
-    this.obtenerDatosFoto(this.datosGenerales.datosPersonales.idUsuario);
-  }
-
-  obtenerDatosFoto(idusuario: number | undefined): void {
-    if (!idusuario) return;
-    this.convocatoriaService.getDatosFotografia(idusuario).subscribe({
-      next: (response: any) => {
-        if (!response.exito) return;
-        this.datosFoto = response.respuesta.fotografia;
-        this.obtenerFotografia()
-      }
-    });
-  }
-
-  obtenerFotografia(): void {
-    if (!this.datosFoto) return;
-    this.documentoService.getFotografia(this.datosFoto.documento.refGuid).pipe(
-    ).subscribe({
-      next: (response: any) => {
-        let extension = ['png', 'jpeg', 'jpg'];
-        if (extension.includes(this.datosFoto.documento.refExtension.toLowerCase())) {
-          this.nombreFoto = this.datosFoto.documento.refNombre;
-          const blob = new Blob([response], {type: 'blob'});
-          this.defaultFile = this.sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(blob));
-        } else {
-          this.nombreFoto = 'Sin foto'
-        }
-      }
-    });
-  }
 
   mostrarDocumento(guid: string) {
     this.documentoService.obtenerDocumento(guid).subscribe({
