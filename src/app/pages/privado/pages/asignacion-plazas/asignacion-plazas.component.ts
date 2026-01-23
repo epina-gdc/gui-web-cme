2802945c7dad6b783e427a8b39c4273e900940a7
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2026-01-22 18:42:53 -0600

    add: components

---
+++ src/app/pages/privado/pages/asignacion-plazas/asignacion-plazas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asignacion-plazas',
+  imports: [],
+  templateUrl: './asignacion-plazas.component.html',
+  styleUrl: './asignacion-plazas.component.scss'
+})
+export class AsignacionPlazasComponent {
+
+}
---
+++ src/app/pages/privado/pages/asignacion-plazas/components/asignacion-sustitucion/asignacion-sustitucion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asignacion-sustitucion',
+  imports: [],
+  templateUrl: './asignacion-sustitucion.component.html',
+  styleUrl: './asignacion-sustitucion.component.scss'
+})
+export class AsignacionSustitucionComponent {
+
+}
