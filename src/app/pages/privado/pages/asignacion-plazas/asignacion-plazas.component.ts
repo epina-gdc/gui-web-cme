32bb163bf122286956697ad59525075b106837b9
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2026-01-26 12:12:04 -0600

    add: maquetado sección COPLAMAR

--- src/app/pages/privado/pages/asignacion-plazas/asignacion-plazas.component.ts
+++ src/app/pages/privado/pages/asignacion-plazas/asignacion-plazas.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import {PlazaOrdinariaComponent} from '@privado/asignacion-plazas/components/plaza-ordinaria/plaza-ordinaria.component';
+import {CoplamarComponent} from '@privado/asignacion-plazas/components/coplamar/coplamar.component';
+import {
+  AsignacionSustitucionComponent
+} from '@privado/asignacion-plazas/components/asignacion-sustitucion/asignacion-sustitucion.component';
+import {TabsModule} from 'primeng/tabs';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-asignacion-plazas',
   imports: [
-    PlazaOrdinariaComponent
+    TabsModule,
+    PlazaOrdinariaComponent,
+    CoplamarComponent,
+    AsignacionSustitucionComponent,
+    CommonModule,
   ],
   templateUrl: './asignacion-plazas.component.html',
   styleUrl: './asignacion-plazas.component.scss'
 })
 export class AsignacionPlazasComponent {
+
+  tab: number = 0;
 
 }
