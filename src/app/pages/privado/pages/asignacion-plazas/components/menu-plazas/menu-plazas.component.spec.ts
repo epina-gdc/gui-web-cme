5ce7ae27572e37d0ebd2e5b16e612ad1e47e0454
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2026-01-26 18:37:25 -0600

    add: Menú asignación

---
+++ src/app/pages/privado/pages/asignacion-plazas/components/menu-plazas/menu-plazas.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MenuPlazasComponent } from './menu-plazas.component';
+
+describe('MenuPlazasComponent', () => {
+  let component: MenuPlazasComponent;
+  let fixture: ComponentFixture<MenuPlazasComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MenuPlazasComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MenuPlazasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
