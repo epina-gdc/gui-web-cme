4e4767545d875b2598e878aa70622ecf9831bb77
Henoch Cruz <henoch.cruz@people-media.com.mx>
2026-02-05 21:18:39 -0600

    Crea componente de detalle convocatoria
    Implementa la vista de detalle de una convocatoria, mostrando el progreso del envío de citas, la última fecha de carga y el total de citas enviadas. Incluye una barra de progreso interactiva simulada.
    
    Realiza ajustes menores de estilo para alinear la altura de las tarjetas en el componente de convocatoria.

--- src/app/pages/privado/pages/asignacion-mesa/envio-citas/components/detalle-convocatoria/detalle-convocatoria.component.ts
+++ src/app/pages/privado/pages/asignacion-mesa/envio-citas/components/detalle-convocatoria/detalle-convocatoria.component.ts
@@ -1,10 +1,36 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { Avatar } from "primeng/avatar";
+import { CardModule } from 'primeng/card';
+import { ProgressBarModule } from 'primeng/progressbar';
 @Component({
   selector: 'app-detalle-convocatoria',
-  imports: [],
+  imports: [CommonModule,CardModule, Avatar, ProgressBarModule],
   templateUrl: './detalle-convocatoria.component.html',
   styleUrl: './detalle-convocatoria.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DetalleConvocatoriaComponent { }
+export class DetalleConvocatoriaComponent implements OnInit, OnDestroy {
+  value = signal(97);
+  private intervalId: number | null = null;
+
+  ngOnInit(): void {
+    this.intervalId = window.setInterval(() => {
+      if (this.value() < 95) {
+        this.value.update(valor => 95);
+      } else {
+
+        this.value.update(valor => (valor + 1) % 101); // 0 a 100
+      }
+
+
+
+    }, 1000); // cada segundo
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId !== null) {
+      clearInterval(this.intervalId);
+    }
+  }
+}
