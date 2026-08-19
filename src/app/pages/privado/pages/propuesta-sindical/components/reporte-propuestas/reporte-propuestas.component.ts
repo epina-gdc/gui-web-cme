81c3b0fff04d108a3022bfa9ede10aec7ead1fdc
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2026-08-19 11:49:10 -0600

    add: componente a estructura de propuesta sindical

---
+++ src/app/pages/privado/pages/propuesta-sindical/components/reporte-propuestas/reporte-propuestas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reporte-propuestas',
+  imports: [],
+  templateUrl: './reporte-propuestas.component.html',
+  styleUrl: './reporte-propuestas.component.scss'
+})
+export class ReportePropuestasComponent {
+
+}
--- src/app/pages/privado/pages/propuesta-sindical/propuesta-sindical.component.ts
+++ src/app/pages/privado/pages/propuesta-sindical/propuesta-sindical.component.ts
@@ -4,10 +4,11 @@ import {TabsModule} from 'primeng/tabs';
 import {GeneralComponent} from '@components/general.component';
 import {FormBuilder} from '@angular/forms';
 import {DatosMedicoComponent} from '@privado/propuesta-sindical/components/datos-medico/datos-medico.component';
+import { ReportePropuestasComponent } from './components/reporte-propuestas/reporte-propuestas.component';
 
 @Component({
   selector: 'app-propuesta-sindical',
-  imports: [CardModule, TabsModule, DatosMedicoComponent],
+  imports: [CardModule, TabsModule, DatosMedicoComponent, ReportePropuestasComponent],
   templateUrl: './propuesta-sindical.component.html',
   styleUrl: './propuesta-sindical.component.scss'
 })
